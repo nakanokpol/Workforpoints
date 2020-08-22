da058be7e3081c5635bfea25dbc716ab13a7c27a
Nakanokpol Sombattheera <nakanokpol49066@gmail.com>
2020-08-23 00:03:24 +0700

    *-*

--- find Greatest Common Divisor/find Greatest Common Divisor/Source.cpp
+++ find Greatest Common Divisor/find Greatest Common Divisor/Source.cpp
@@ -3,20 +3,33 @@ int maxvalue(int, int);
 
 int main()
 {
-	int a, b, x = 1;
+	int a, b, x = 1, c, d ,e,f;
 	printf("Enter first number : ");
 	scanf_s("%d", &a);
 	printf("Enter second number : ");
 	scanf_s("%d", &b);
 
+	c = a;
+	d = b;
+
 	printf("Greatest common divisor : ");
+	if (a < 0)
+	{
+		a = -a;
+	}
+	if (b < 0)
+	{
+		b = -b;
+	}
 	if (a == 0 || b == 0)
 	{
 		x = 0;
+		printf("%d", x);
+		return 0;
 	}
-	else
+	else if (a != 0 && b != 0)
 	{
-		for (int i = 1; i <= maxvalue(a, b); ) 
+		for (int i = 2; i <= maxvalue(a, b); ) 
 		{
 			if (a % i == 0 && b % i == 0) 
 			{
@@ -30,8 +43,15 @@ int main()
 			}
 		}
 	}
-	
-	printf("%d", x);
+	if ((c > 0 && d > 0) || c*d<0)
+	{
+		printf("%d", x);
+	}
+	else if (c < 0 && d < 0)
+	{
+		printf("%d", -x);
+	}
+
 	return 0;
 }
 
