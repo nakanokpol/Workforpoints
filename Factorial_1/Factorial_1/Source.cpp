8e191bb62c2cca31f05a8b6a7fcd192e622250d1
Nakanokpol Sombattheera <nakanokpol49066@gmail.com>
2020-08-22 11:53:37 +0700

    *-*

---
+++ Factorial_1/Factorial_1/Source.cpp
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int x;
+	printf("Enter number : ");
+	scanf("%d", &x);
+	if (x == 0 || x == 1)
+	{
+		printf("Factorial Result : %d", x);
+	}
+	else
+	{
+		printf("Factorial Result : ");
+		if (x < 0)
+		{
+			printf("-1 x ");
+			x = -x;
+		}
+		for (int i = 2; i <= x;)
+		{
+			if (x % i == 0)
+			{
+				printf("%d", i);
+				x /= i;
+				if (x != 1)
+				{
+					printf(" x ");
+				}
+			}
+			else
+			{
+				i++;
+			}
+		}
+	}
+	return 0;
+}
