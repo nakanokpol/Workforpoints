2b6fa6c79c4775de002f9496f7f09614499e992f
Nakanokpol Sombattheera <nakanokpol49066@gmail.com>
2020-08-30 15:20:44 +0700

    *-*

---
+++ Lab3_work1/Lab3_work1/Source.cpp
@@ -0,0 +1,103 @@
+#include<stdio.h>
+
+//int b(int);
+int a(int);
+
+int main()
+{
+	int num;
+	scanf_s("%d", &num);
+
+	printf("%d", a(num));
+	return 0;
+}
+
+int a(int num)
+{
+	int R=0;
+	if (num >= 10)
+	{
+		for (; num != 0 ; num /= 10)
+		{
+			R += num % 10;
+		}
+		a(R);
+	}
+	else
+	{
+		return num;
+	}
+}
+
+/*int main()
+{
+	int sum;
+	
+	for (int i = 1; i <= 10000; i++)
+	{
+		if (i == b(i))
+		{
+			printf("%d\n",i);
+		}
+	}
+
+}
+
+int b(int num)
+{
+	int sum = 0;
+	for (int i = 1; i <= num - 1; i++)
+	{
+		if (num % i == 0)
+		{
+			sum += i;
+			//if(num == 496 || num ==8128)
+			//printf("i = %d\n", i);
+		}
+		//printf("sum = %d\n", sum);
+	}
+	return sum;
+}*/
+
+/*int main()
+{
+	int x;
+	scanf_s("%d", &x);
+	for (int i = 1; i <= x -1 ; i++) // 1. บรรทัดลูปแรก
+	{
+		for (int k = 1; k <= i; k++)
+		{
+			printf("* ");
+		}
+		for (int k = 1; k <= (2 * x) - (2*i+1); k++)// บรรทัดลูปสอง
+		{
+			printf("  ");
+		}
+		for (int k = 1; k <= i; k++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+	for (int i = 1; i <= (2 * x) - 1; i++)
+	{
+		printf("* ");
+	}
+	printf("\n");
+	for (int i = x-1; i >= 1 ; i--) // บรรทัดลูปสาม
+	{
+		for (int k = i; k >=1; k--)
+		{
+			printf("* ");
+		}
+		for (int k = (2 * x) - (2 * i + 1); k>=1 ; k--)//
+		{
+			printf("  ");
+		}
+		for (int k = i; k >= 1; k--)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+}*/
---
+++ sum of ditgit/sum of ditgit/Source.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int a(int);
+
+int main()
+{
+	int num;
+	scanf_s("%d", &num);
+
+	printf("%d", a(num));
+	return 0;
+}
+
+int a(int num)
+{
+	int R = 0;
+	if (num >= 10)
+	{
+		for (; num != 0; num /= 10)
+		{
+			R += num % 10;
+		}
+		a(R);
+	}
+	else
+	{
+		return num;
+	}
+}
