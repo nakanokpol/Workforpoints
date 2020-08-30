fcdfdbfd85b8ca154f14b8759f3072c97666db19
Nakanokpol Sombattheera <nakanokpol49066@gmail.com>
2020-08-30 15:27:55 +0700

    -*-

--- sum of ditgit/sum of ditgit/Source.cpp
+++ sum of ditgit/sum of ditgit/Source.cpp
@@ -11,16 +11,16 @@ int main()
 	return 0;
 }
 
-int a(int num)
+int a(int num) // initial of Loop
 {
 	int R = 0;
-	if (num >= 10)
+	if (num > 10 || num == 10)
 	{
 		for (; num != 0; num /= 10)
 		{
 			R += num % 10;
 		}
-		a(R);
+		a(R);// Loop
 	}
 	else
 	{
