f8e1079d09e49177054ce51f93cd14f2f95d8ab2
Nakanokpol Sombattheera <nakanokpol49066@gmail.com>
2020-08-30 16:06:31 +0700

    *-*

---
+++ Bow/Bow/Source.cpp
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+int main()
+{
+	int x;
+	scanf_s("%d", &x);
+	for (int i = 1; i <= x -1 ; i++)
+	{
+		for (int k = 1; k <= i; k++)
+		{
+			printf("* ");
+		}
+		for (int k = 1; k <= (2 * x) - (2*i+1); k++)
+		{
+			printf("  ");
+		}
+		for (int k = 1; k <= i; k++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+	for (int i = 1; i <= (2 * x) - 1; i++)
+	{
+		printf("* ");
+	}
+	printf("\n");
+	for (int i = x-1; i >= 1 ; i--)
+	{
+		for (int k = i; k >=1; k--)
+		{
+			printf("* ");
+		}
+		for (int k = (2 * x) - (2 * i + 1); k>=1 ; k--)
+		{
+			printf("  ");
+		}
+		for (int k = i; k >= 1; k--)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+}
