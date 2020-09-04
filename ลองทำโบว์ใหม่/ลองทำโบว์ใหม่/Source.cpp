754d2910c07f65c12dec49ba9957b4db2d715d08
Nakanokpol Sombattheera <nakanokpol49066@gmail.com>
2020-09-04 21:33:11 +0700

    *-*

---
+++ ลองทำโบว์ใหม่/ลองทำโบว์ใหม่/Source.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf_s("%d", &n);
+
+	int l = (2 * n) - 1;
+	for (int i = 1; i <= l; i++)
+	{
+		for (int k = 1; k <= l;k++)
+		{
+			if ((k <= i || k > l - i) && i<=n)
+			{
+				printf("*");
+			}
+			else if (i <= n)
+			{
+				printf("x");
+			}
+			if ((k <= l-(i-1) || k > i-1) && i>n)
+			{
+				printf("*");
+			}
+			else if (i > n)
+			{
+				printf("x");
+			}
+		}
+		printf("\n");
+	}
+}
