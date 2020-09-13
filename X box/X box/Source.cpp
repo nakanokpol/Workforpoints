54fc7b81d2a91b58ac03bf1d5d983598337ad046
Nakanokpol Sombattheera <nakanokpol49066@gmail.com>
2020-09-13 17:50:26 +0700

    *-*

--- X box/X box/Source.cpp
+++ X box/X box/Source.cpp
@@ -1,16 +1,21 @@
 #include<stdio.h>
-#include<conio.h>
+
+void XBOX(int);
 
 void main()
 {
 	int n;
 	scanf_s("%d", &n);
+	XBOX(n);
+}
 
+void XBOX(int n)
+{
 	for (int i = 1; i <= n; i++)
 	{
 		for (int j = 1; j <= n;j++)
 		{
-			if (i==1||i==n||j==1||j==n||i==j||i==n-j+1)
+			if (i == 1 || i == n || j == 1 || j == n || i == j || i == n - j + 1)
 			{
 				printf("* ");
 			}
