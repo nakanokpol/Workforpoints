808c62597c44a260c9b9faa6d574f772a44db53f
Nakanokpol Sombattheera <nakanokpol49066@gmail.com>
2020-10-25 23:56:41 +0700

    *-*

---
+++ 555/555/Source.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	int a[5], b;
+	for (int i = 0; i <= 4;i++)
+		scanf_s("%d", &a[i]);
+
+	scanf_s("%d",&b);
+
+	if(b>0 && b<=4)
+		for (int j = b-1; j <= 4; j++)
+			printf("%d = %d\n",j, a[j]);
+
+	else if(b < 0 && abs(b)<= 4)
+		for (int j = 4; j >= 5-b; j--)
+			printf("%d = %d\n",j, a[j]);
+
+	else
+	printf("last input is int -5 to 5 except 0 only")
+
+	return 0;
+}
