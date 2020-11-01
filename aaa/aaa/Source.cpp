47ef9fceec6f5dde0ad15ebbc8aca17cfe05eaab
Nakanokpol Sombattheera <nakanokpol49066@gmail.com>
2020-11-01 22:12:47 +0700

    *-*

--- aaa/aaa/Source.cpp
+++ aaa/aaa/Source.cpp
@@ -4,6 +4,8 @@ int main()
 {
 	char a[100];
 	char b[100];
+	char checkNum[100];
+
 	printf("Your Inputs : ");
 	scanf_s("%s", a, 99);
 
@@ -12,47 +14,52 @@ int main()
 	printf("Your Characters : ");
 
 	int i = 0;
+	int k = 0;
+	int t = 0;
 
 	while (*p != '\0')
 	{
-		
-		if ((*p > 'a' && *p < 'z') || (*p > 'A' && *p < 'Z'))
+		if ((*p >= 'a' && *p <= 'z') || (*p >= 'A' && *p <= 'Z'))
 		{
 			printf("%c", *p);
 			b[i] = *p;
 			i++;
 		}
+		if (*p >= '0' && *p <= '9')
+		{
+			checkNum[t] = *p;
+			t++;
+		}
+
 		p++;
+		k++; // the number of loop
+	}
+
+	if (k == t) // จำนวนตัวเลขเท่ากับจำนวนอินพุดทั้งหมด แสดงว่า input ที่รับมาทั้งหมดเป็นตัวเลข
+	{
+		printf("\nnot Palindrome");
+		return 0;
+	}
+	
+	char* q = b;
+
+	if (k >= 1)
+	{
+		--i;//i บวกท้ายลูปสุดท้าย เลขเลยเกินมา 1
 	}
 	if (i >= 1)
 	{
 		--i;//i บวกท้ายลูปสุดท้าย เลขเลยเกินมา 1
 	}
-
 	
-	char* q = b;
-
-	if (i == 0)
+	for (int j = 0; j <= i; j++)
 	{
-		printf("\nERROR::DON'T_HAVE_ANY_CHARACTER\nnot Palindrome");
-		return 0;
-	}
-	else
-	{
-		for (int j = 0; j <= i; j++)
+		if (q[j] != *(q + i - j))
 		{
-			if ((i % 2 == 1) || ((i % 2 == 0) && (j != i / 2))) // (มีข้อมูลจำนวนคี่ตัว && i เป็นจำนวนคู่) || (มีข้อมูลจำนวนคู่ตัว) 
-			{
-				if (q[j] != *(q + i - j))
-				{
-					printf("\nnot Palindrome");
-					return 0;
-				}
-			}
+			printf("\nnot Palindrome");
+			return 0;
 		}
 	}
-
 	printf("\nPalindrome");
-
 	return 0;
 }
